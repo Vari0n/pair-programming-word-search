1bc34f9eca715e840f710c6864ba7d7228dfd892
Vari0n <Kenly_P@yahoo.ca>
2020-11-04 22:50:49 +0000

    implemented a vertical word search function to analyze a 2d array vertically if it has a particular word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
+const transpose = function(matrix) { 
+    let resultArr = []
+    
+      for (let i = 0; i < matrix[0].length; i++) {
+        let innerArr = []
+        // let eatchArr = matrix[i]
+        // resultArr.push(eatchArr[0])
+        
+        for (let value of matrix){
+          innerArr.push(value[i])
+        }
+  
+        resultArr.push(innerArr);
+    }
+    return resultArr  
+  };
+
+
+
 const wordSearch = (letters, word) => { 
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        console.log(l);
+        if (l.includes(word)){
+            return true
+        } 
+            
     }
+    const arr = transpose(letters)
+    const verticalJoin = arr.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        console.log(l);
+        if (l.includes(word)){
+            return true
+        }
+    }
+return false;   
 }
 
-module.exports = wordSearch
+
+ module.exports = wordSearch;
